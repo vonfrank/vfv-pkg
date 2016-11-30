ebcc73eaa34cef609f367bb1d380cf69629089d4
Henrik Bolding Frank <henrik@vonfrank.dk>
2016-11-30 01:26:48 +0100

    static colors working

--- src/app/heatmap/heatmap.component.ts
+++ src/app/heatmap/heatmap.component.ts
@@ -4,7 +4,6 @@ import { DataObject } from '../models/data-object';
 import { Metadata } from '../models/metadata';
 import { Data } from '../models/data';
 
-
 @Component({
   selector: 'app-heatmap',
   templateUrl: './heatmap.component.html',
@@ -13,11 +12,15 @@ import { Data } from '../models/data';
 export class HeatmapComponent {
   title = "Heatmap";
 
+  color = "96,125,139";
+
   metaArray: Metadata[];
   dataSetArray: Object[][];
   dataArray: Object[];
 
   xArray: Object[];
+
+  maxValue: number;
 
   //Injecting common data service, provided by app.component
   constructor(private dataService: DataService) { }
@@ -28,11 +31,13 @@ export class HeatmapComponent {
     
     this.xArray = [];
 
+    this.maxValue = 0;
+
     this.getMetadata();
     this.getDataSet();
   }
 
-    //onSelect action from html file, runs every time, a metadata has been pressed
+  //onSelect action from html file, runs every time, a metadata has been pressed
   onSelect(selectedMeta: Metadata): void {
     this.dataService.selectMetadata(selectedMeta).then(metaArray => this.metaArray = metaArray);
     this.getDataSet();
@@ -48,6 +53,14 @@ export class HeatmapComponent {
   //Gets the entire data set, from the injected data service
   private getDataSet(): void {
     this.dataService.getDataSet().then(dataSet => this.dataSetArray = dataSet);
+    this.maxValue = 0;
+    for(var i: number = 1; i < this.dataSetArray.length; i++) {
+      for(var j: number = 1; j < this.dataSetArray[i].length; j++) {
+        if(<number>this.dataSetArray[i][j] > this.maxValue) {
+          this.maxValue = <number>this.dataSetArray[i][j];
+        }
+      }
+    }
   }
 
   private generateViewX(): void {
